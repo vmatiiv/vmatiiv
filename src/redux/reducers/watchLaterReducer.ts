50a726d17e701ef14e6cf852cc8dd9bb07ae0788
vmatiiv <matiiv.vasyl97@gmail.com>
2020-06-18 22:21:23 +0300

    delete console logs

--- src/redux/reducers/watchLaterReducer.ts
+++ src/redux/reducers/watchLaterReducer.ts
@@ -7,7 +7,6 @@ export const watchLaterReducer = (initialState:any) => (state=initialState,actio
     switch (action.type) {
         case ADD_TO_WATCH_LIST:
 
-            console.log([action.payload])
             return [...state,action.payload]
 
         case REMOVE_FROM_WATCH_LIST:
